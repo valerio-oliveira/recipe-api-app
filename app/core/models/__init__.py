32af555723c7191fdd333a07566156043d22ddc2
Valerio Oliveira <valerio.net@gmail.com>
2024-02-11 03:01:39 -0300

    Code sanitizing.

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,4 +1,4 @@
-from .user import User # flake8: noqa
-from .user_manager import UserManager # flake8: noqa
-from .recipe import Recipe # flake8: noqa
-from .tag import Tag # flake8: noqa
+from .user import User  # flake8: noqa
+from .user_manager import UserManager  # flake8: noqa
+from .recipe import Recipe  # flake8: noqa
+from .tag import Tag  # flake8: noqa
